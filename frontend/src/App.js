7455258d530931786a4ee8fc18fbd940cb4c3244
Comanoiu Mihai <mihaicomanoiu2017@gmail.com>
2022-08-27 15:24:57 +0300

    Routing and created some pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
 import React from 'react'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
+import Navbar from './components/Navbar'
+
 const App = () => {
-  return <div>App</div>
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='sign-up' element={<SignUp />} />
+        <Route path='sign-in' element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styled from 'styled-components'
+
+const Container = styled.div`
+  background-color: red;
+`
+
+const Navbar = () => {
+  return <Container>Test</Container>
+}
+
+export default Navbar
